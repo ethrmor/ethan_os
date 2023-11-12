21ff989b81ff29ddc4ff36b875763fd140f392c0
Ethan Morris <pirates.postbox-0t@icloud.com>
2023-11-11 22:16:40 -0600

    chore(package.json): update next.js version to 14.0.2 and react/react-dom versions to 18.2.0 for compatibility and latest features chore(package.json): update @types/react and @types/react-dom versions to 18.2.37 and 18.2.15 respectively for updated type definitions chore(package.json): update eslint-config-next version to 14.0.2 for updated linting rules fix(page.tsx): adjust top padding of h1 element to improve spacing and alignment

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   const projects = await getProjects();
   return (
     <section className="flex flex-1 flex-col gap-2 py-8">
-      <h1 className="pb-4 pt-20 text-3xl font-extrabold text-zinc-900 dark:text-zinc-50">
+      <h1 className="pb-4 pt-12 text-3xl font-extrabold text-zinc-900 dark:text-zinc-50">
         Problem solver. Creative thinker. Software Developer.
       </h1>
       <p className="text-md pb-12 text-zinc-600 dark:text-zinc-400">
