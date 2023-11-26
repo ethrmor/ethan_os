bd0bbcf6aaa09d94aab9812e566502a9066d0f67
Ethan Morris <pirates.postbox-0t@icloud.com>
2023-11-26 10:08:36 -0600

    chore(icons.tsx): add externalLink icon to the Icons component for use in project cards refactor(page.tsx): update imports and component names in the site/page.tsx file to match changes in project-card component refactor(page.tsx): update styling and content in the site/page.tsx file to improve readability and user experience refactor(projects/page.tsx): update imports and component names in the projects/page.tsx file to match changes in project-card component refactor(projects/page.tsx): update styling and content in the projects/page.tsx file to improve readability and user experience refactor(project-card.tsx): remove unused component and update project card implementation in site/page.tsx and projects/page.tsx

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,28 +1,129 @@
-import { getProjects } from "../../sanity/utils";
-import ProjectCard from "@/components/project-card";
+import Image from "next/image";
+import Link from "next/link";
+
+import { getProjects, getBlogs } from "../../sanity/utils";
+import { Icons } from "@/components/icons";
 
 export default async function Home() {
   const projects = await getProjects();
+  const blogs = await getBlogs();
   return (
     <section className="flex flex-1 flex-col gap-2 py-8">
       <h1 className="pb-4 pt-12 text-3xl font-extrabold text-zinc-900 dark:text-zinc-50 md:text-5xl">
         Problem solver. Creative thinker.{" "}
-        <span className="bg-gradient-to-r from-purple-900 to-sky-700 bg-clip-text text-transparent dark:from-purple-700 dark:to-sky-500">
+        <span className="bg-gradient-to-r from-purple-900 to-purple-700 bg-clip-text text-transparent dark:from-purple-700 dark:to-purple-500">
           Software Developer
         </span>
         .
       </h1>
       <p className="text-md pb-12 text-zinc-600 dark:text-zinc-400">
-        I&apos;m Ethan, a software developer based in Illinois. I&apos;m
-        passionate about the web and I love building things.
+        Hi 👋🏻 I&apos;m Ethan. Welcome to my tiny little corner of the internet.
+        I am a software developer based in Illinois who is passionate about all
+        things web. I love to build things, whether it&apos;s with my hands or
+        with code.
       </p>
       <br />
-      <h2>Projects</h2>
-      <div className="grid gap-2 md:grid-cols-2">
+      <h2 className="text-lg font-bold md:text-2xl">Selected Projects</h2>
+      <div className="grid gap-2 pb-12">
         {projects.map((project) => (
-          <ProjectCard key={project._id} project={project} />
+          <article
+            key={project._id}
+            className="overflow-hidden rounded-lg shadow transition @container hover:shadow-lg @md:grid-cols-5 dark:hover:shadow-black"
+          >
+            <div className="grid grid-cols-1 @md:grid-cols-5">
+              <div className="col-span-2">
+                {project.image && (
+                  <Image
+                    src={project.image}
+                    alt={project.alt ? project.alt : project.name}
+                    width={1600}
+                    height={900}
+                    className="h-full w-full object-cover"
+                  />
+                )}
+              </div>
+              <div className="col-span-3 bg-white p-4 dark:bg-zinc-900 sm:p-6">
+                <Link
+                  href={`/projects/${project.slug}`}
+                  className="w-full @container"
+                >
+                  <h3 className="text-pretty mt-0.5 text-lg text-zinc-900 dark:text-zinc-50">
+                    {project.name}
+                  </h3>
+                </Link>
+
+                <p className="mt-3 line-clamp-3 text-sm/relaxed text-zinc-600 dark:text-zinc-300">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Recusandae dolores, possimus pariatur animi temporibus
+                  nesciunt praesentium dolore sed nulla ipsum eveniet corporis
+                  quidem, mollitia itaque minus soluta, voluptates neque
+                  explicabo tempora nisi culpa eius atque dignissimos. Molestias
+                  explicabo corporis voluptatem?
+                </p>
+                <div className="mt-3 flex items-center gap-8">
+                  <Link
+                    href="https://www.github.com/ethrmor/chug-official"
+                    className="flex items-center gap-2 text-xs hover:opacity-80"
+                  >
+                    <Icons.gitHub className="h-5 w-5" />
+                    <span className="underline">View on GitHub</span>
+                  </Link>
+                  <Link
+                    href="https://chug-official.vercel.app"
+                    className="flex items-center gap-2 text-xs hover:opacity-80"
+                  >
+                    <Icons.externalLink className="h-5 w-5" />
+                    <span className="underline">View Live Site</span>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </article>
+        ))}
+      </div>
+      <h2 className="text-lg font-bold md:text-2xl">Latest Writings</h2>
+      <div className="flex flex-col gap-2">
+        {blogs.map((blog) => (
+          <article key={blog._id}>
+            <div className="sm:py-6">
+              <time
+                dateTime="2022-10-10"
+                className="block text-xs text-zinc-600 dark:text-zinc-300"
+              >
+                {prettifyDate(blog._createdAt)}
+              </time>
+              <Link href={`/blog/${blog.slug}`} className="font-bold underline">
+                <h3 className="text-pretty mt-0.5 text-lg text-zinc-900 dark:text-zinc-50">
+                  {blog.name}
+                </h3>
+              </Link>
+              <p className="mt-2 line-clamp-3 text-sm/relaxed text-zinc-600 dark:text-zinc-300">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Recusandae dolores, possimus pariatur animi temporibus nesciunt
+                praesentium dolore sed nulla ipsum eveniet corporis quidem,
+                mollitia itaque minus soluta, voluptates neque explicabo tempora
+                nisi culpa eius atque dignissimos. Molestias explicabo corporis
+                voluptatem?
+              </p>
+              <Link
+                href={`/blog/${blog.slug}`}
+                className="mt-3 flex items-center gap-2 text-xs underline hover:opacity-90"
+              >
+                Read More
+              </Link>
+            </div>
+          </article>
         ))}
       </div>
     </section>
   );
+}
+
+function prettifyDate(date: string) {
+  const d = new Date(date);
+  return d.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
 }
--- src/app/(site)/projects/page.tsx
+++ src/app/(site)/projects/page.tsx
@@ -1,5 +1,7 @@
-import ProjectCard from "@/components/project-card";
+import { Icons } from "@/components/icons";
 import { getProjects } from "@/sanity/utils";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function Projects() {
   const projects = await getProjects();
@@ -9,7 +11,59 @@ export default async function Projects() {
         <h1 className="text-zinc-900 dark:text-zinc-50">Projects</h1>
         <div className="not-prose flex flex-col gap-4">
           {projects.map((project) => (
-            <ProjectCard key={project._id} project={project} />
+            <article
+              key={project._id}
+              className="overflow-hidden rounded-lg shadow transition @container hover:shadow-lg @md:grid-cols-5 dark:hover:shadow-black"
+            >
+              <div className="grid grid-cols-1 @md:grid-cols-5">
+                <div className="col-span-2">
+                  {project.image && (
+                    <Image
+                      src={project.image}
+                      alt={project.alt ? project.alt : project.name}
+                      width={1600}
+                      height={900}
+                      className="h-full w-full object-cover"
+                    />
+                  )}
+                </div>
+                <div className="col-span-3 bg-white p-4 dark:bg-zinc-900 sm:p-6">
+                  <Link
+                    href={`/projects/${project.slug}`}
+                    className="w-full @container"
+                  >
+                    <h3 className="text-pretty mt-0.5 text-lg text-zinc-900 dark:text-zinc-50">
+                      {project.name}
+                    </h3>
+                  </Link>
+
+                  <p className="mt-3 line-clamp-3 text-sm/relaxed text-zinc-600 dark:text-zinc-300">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Recusandae dolores, possimus pariatur animi temporibus
+                    nesciunt praesentium dolore sed nulla ipsum eveniet corporis
+                    quidem, mollitia itaque minus soluta, voluptates neque
+                    explicabo tempora nisi culpa eius atque dignissimos.
+                    Molestias explicabo corporis voluptatem?
+                  </p>
+                  <div className="mt-3 flex items-center gap-8">
+                    <Link
+                      href="https://www.github.com/ethrmor/chug-official"
+                      className="flex items-center gap-2 text-xs hover:opacity-80"
+                    >
+                      <Icons.gitHub className="h-5 w-5" />
+                      <span className="underline">View on GitHub</span>
+                    </Link>
+                    <Link
+                      href="https://chug-official.vercel.app"
+                      className="flex items-center gap-2 text-xs hover:opacity-80"
+                    >
+                      <Icons.externalLink className="h-5 w-5" />
+                      <span className="underline">View Live Site</span>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </article>
           ))}
         </div>
       </section>
